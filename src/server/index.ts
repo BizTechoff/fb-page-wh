df8fe9b348bdf09e1843ee6c05c66db548bc335f
Motidru <motidru@gmail.com>
2024-08-05 14:00:34 +0300

    fb event

--- src/server/index.ts
+++ src/server/index.ts
@@ -30,8 +30,8 @@ async function startup() {
   }
   app.use(express.static(dist))
 
-  app.post('/api/fb/webhook', async (req, res) => {
-    console.log(`fb.webhook called at: ${new Date()}`)
+  app.get('/api/fb/webhook', async (req, res) => {
+    console.log(`get.fb.webhook called at: ${new Date()}`)
     let result: { success: boolean, error: string } = { success: false, error: '' };
     let key = req.query['key'] as string;
 
@@ -65,6 +65,25 @@ async function startup() {
     // res.send(challenge);
     res.status(200).send(challenge);
   })
+
+  // Endpoint to handle webhook events
+  app.post('/api/fb/webhook', async (req, res) => {
+    const body = req.body;
+    console.log('post.fb.body', JSON.stringify(body))
+
+    if (body.object === 'page') {
+      body.entry.forEach(function (entry: any) {
+        const webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+        // Handle the event here
+      });
+
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      res.sendStatus(404);
+    }
+  });
+
 
   app.use('/*', async (req, res) => {
     if (req.headers.accept?.includes('json')) {
